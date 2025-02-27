61af51294733a3b3cfd6ee80afc5e5ac60704272
TWeapon <98501369+TWeapon@users.noreply.github.com>
2025-02-27 19:25:23 +0800

    2-27
    初次创建
    1.快捷键
    2.pytorch pdf

---
+++ pythonStaudy/first.py
@@ -0,0 +1,11 @@
+"""import this
+
+print(this.d)"""
+
+
+def f(x, y, w):
+    print(2 * x + 3 * y - w + 4)
+
+
+f(1, 2, 3)
+#有内置函数 builtins模块里有内置函数 直接用
---
+++ pythonStaudy/p04_标准数据类型，索引&切片.py
@@ -0,0 +1,106 @@
+# int([x],base=10)
+# x:数字或字符串
+# base:进制数，默认十进制（当base传参时，x必须为字符串）
+# 将x转换为十进制整数并返回，如果没有指定x,则返回0
+print(int())  # 0
+print(int(3.99))  # 3
+print(int("12"))  # 12
+
+# ValueError: 字符串中包含了一个小数点，无法被解释为整数
+# print(int("12.1"))
+
+# 将二进制的“101010”转为十进制的整数
+print(int("101010", base=2))  # 42
+print(int("0b101010", 2))  # 42
+# 将八进制的“11”转为十进制的整数
+print(int("11", base=8))  # 9
+print(int("0o11", base=8))  # 9
+# 将十六进制的“3d”转为十进制的整数
+print(int('3d', base=16))  # 61
+print(int("0x3d", base=16))
+
+# bin(x)/ oct(x) / hex(x)
+# 返回整数类型的二进制表示形式（前缀为“0b”）
+# 返回整数类型的八进制表示形式（前缀为“0o”）
+# 返回整数类型的十六进制表示形式（前缀为“0x”）
+num = 47
+print(bin(num))  # '0b101111'
+print(oct(num))  # '0o57'
+print(hex(num))  # '0x2f'
+print(int("0b101111", base=2))
+print(int("0o57", base=8))
+print(int("0x2f", base=16))
+
+# float([x])
+# x:数字或字符串
+# 将x转换成浮点数并返回，不指定x,则返回0.0
+print(float())  # 0.0
+print(float(123))  # 123.0
+print(float('123'))  # 123.0
+print(float('1.23'))  # 1.23
+print(float('3.99'))  # 3.99
+print(float('-3.99'))  # -3.99
+print(float(True))  # 1.0
+print(float(False))  # 0.0
+
+# bool([x])
+# 根据指定的x,返回布尔值
+# 如果没有指定x,则返回False
+"""
+数字0,0.0,0j,False,
+空字符串,空列表,空元组,
+空字典,空集合,关键字None
+...
+以上这些数据bool判定为False
+其它通常判定为True
+"""
+print(bool())  # False
+print(bool(0))  # False
+print(bool(0.0))  # False
+print(bool(0.0))  # False
+print(bool(0j))  # False
+print(bool(False))  # False
+print(bool(''))  # False
+print(bool([]))  # False
+print(bool(()))  # False
+print(bool({}))  # False
+print(bool(set()))  # False
+print(bool(None))  # False
+print(bool(' '))  # True
+print(bool('None'))  # True
+print(bool('False'))  # True
+
+# complex([real[, imag]])
+# 返回一个real+imag*1j的复数
+# 如果第一个参数是字符串，它将被解释为复数，此时不能传第二个参数
+# 如果没有指定实参，则返回0j
+print(complex())  # 0j
+print(complex(3.2, 4))  # (3.2+4j)
+print(complex(3.2))  # (3.2+0j)
+print(complex('3.2'))  # (3.2+0j)
+print(complex("3.2+4j"))  # (3.2+4j)
+
+# 字符串（String）
+# 特性：不可变，是序列
+# 单行字符串：用一对单引号或一对双引号定义
+# 多行字符串：用三个单引号或三个双引号定义
+s1 = '这是一个单行字符串'
+s2 = "这是一个单行字符串"
+s3 = '''这是一个单行字符串'''
+s4 = """这是一个单行字符串"""
+print(s1, s2, s3, s4)
+# str(object='')
+# 返回object的字符串格式
+print(str())    # '' 没有空格字符
+print(str(1234))    # '1234'
+print(str(-1.23))   # '-123'
+
+# Raw 字符串
+# 如果希望字符串中的转义字符不发生转义效果，可以在字符串前面加一个字母r，表示原始字符串
+
+print(R'https:\\www.example.com\nuxy\tngj')
+
+# %格式化
+# %s 格式化为字符串
+# %d、%i 格式化为十进制整数，仅适用于数字
+# %f、%F 格式化为浮点数，默认精确到小数点后六位，仅适用于数字
